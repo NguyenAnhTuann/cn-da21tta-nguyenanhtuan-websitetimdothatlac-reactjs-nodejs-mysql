958ae34615c9c13cdd780bb9daa3f6e1bf0fdf1c
ngg_anhtuan <ng_anhtuan@outlook.com>
2024-11-09 17:35:59 +0700

    Tạo routes, test cổng 5000

---
+++ src/websitetimdothatlac/server/server.js
@@ -0,0 +1,20 @@
+import cors from 'cors'
+import express from 'express'
+import initRouters from './src/routes'
+require('dotenv').config()
+
+
+const app = express()
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["POST", "GET", "PUT", "DELETE"]
+}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+initRouters(app)
+
+const port = process.env.PORT || 8888
+const listener = app.listen(port, () => {
+    console.log(`Server is running on the port ${listener.address().port}`)
+})
